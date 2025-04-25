3d492fe8e5fabc6964555a25bd416f0cbdd3e177
AllenDufort <allen_dufort@brown.edu>
2025-04-25 16:00:20 -0400

    Revert "Update run_persistent_LLM.sh"
    This reverts commit 649b047149b9657f7f4205981f78d6d32f24ce72.

--- run_persistent_LLM.sh
+++ run_persistent_LLM.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#SBATCH -n 1
-#SBATCH --mem=128G
-#SBATCH -t 06:00:00
+#SBATCH -n 4
+#SBATCH --mem=16G
+#SBATCH -t 00:30:00
 #SBATCH -p gpu --gres=gpu:1
 
-# Install requirements in the virtual environment
 pip install -r requirements.txt
-pip install huggingface_hub
-pip install transformers
-pip install torch
-python -u ./Persistent_LLM_Rewards.py
+python -u ./Persistent_LLM_Rewards.py --num_eps 1000 --save_every 50 --max_dynamic_tokens 4096
